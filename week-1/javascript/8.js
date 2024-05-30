fc9e99a782b67cbd9faef0647d89b9d6f32436e8
Marian Adonay <marianadonay40@gmail.com>
2024-05-30 14:45:41 +0800

    Write a function that returns all vowels in a string.

--- week-1/javascript/8.js
+++ week-1/javascript/8.js
@@ -1,5 +1,21 @@
 /**
  * Write a function that returns all vowels in a string.
  * @param {string} str The string to check.
- * @return {string} The string with all vowels removed.
+ * @return {string} The string with all vowels.
  */
+
+
+function Vowels (str){
+    const vowels = ["a", "e","i","o", "u" ];
+    let value = '';
+
+    for (let i of str.toLowerCase()){
+        if(vowels.includes(i)){
+            value += i
+        }
+    }
+    return value;
+
+}
+
+console.log(Vowels("Maglisod ko"));

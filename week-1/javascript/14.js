67475f6a36c52bc6624ff9bc5c42aff40cc26435
Marian Adonay <marianadonay40@gmail.com>
2024-05-31 09:46:02 +0800

    [JS-14] Write a function that accepts an array of numbers and displays the highest and lowest numbers in the array.

--- week-1/javascript/14.js
+++ week-1/javascript/14.js
@@ -6,3 +6,19 @@
  * Example array: [5, 2, 8, 1, 9, 3]
  * Expected output: "The lowest number is 1 and the highest number is 9."
  */
+
+function number(arr){
+    if (arr.length ===0){
+        throw new Error ("Empty Array")
+    }
+        const highestNumber = Math.max(...arr);
+        const lowestNumber = Math.min(...arr);
+    return(`The lowest number is ${lowestNumber} and the highest number is ${highestNumber}`)
+}
+try{
+    let numberarray = number ([ 5,46,45,7,8])
+    console.log(numberarray)
+}
+catch(err){
+    console.log(err.message)
+}

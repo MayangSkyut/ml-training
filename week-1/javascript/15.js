10934c86a7abed85ab22fb1aac06638252edf9c3
Marian Adonay <marianadonay40@gmail.com>
2024-05-31 13:10:17 +0800

    [JS-15] Write a function for loop that iterates from 1 to a given number. It then checks and outputs whether the number is even or odd.

--- week-1/javascript/15.js
+++ week-1/javascript/15.js
@@ -2,3 +2,28 @@
  * Write a function for loop that iterates from 1 to a given number. It then checks and outputs whether the number is even or odd.
  * @param {number} n The number to check.
  */
+
+/// maghatag kog number then starting from 1 mag iterate og number hangtud sa ma stop and iteration sa number na akong gihatag.Dayun mo chech
+/// nga akong gihatag if even ba or odd.
+
+
+function EvenOddnum (n){
+  for (let number = 1; number < n ; number++ ){
+       if (number % 2 === 0 ){
+        console.log(`${number} is even number.`)
+       }
+       else {
+        console.log(`${number} is odd number.`)
+       }
+
+    }      
+}
+
+   EvenOddnum(20);     
+
+
+
+
+
+
+

e43422826a4ec44ed2a8ccae20c2c8e415650e8c
Marian Adonay <marianadonay40@gmail.com>
2024-06-01 11:04:20 +0800

    [TS-6] Write a function that converts Fahrenheit to Celsius.

--- week-1/typescript/6.ts
+++ week-1/typescript/6.ts
@@ -4,3 +4,11 @@
  * @param {number} fahrenheit The temperature in Fahrenheit.
  * @return {number} The temperature in Celsius.
  */
+
+
+function TemperatureFahrenheit(fahrenheit: number): number{
+    return ((fahrenheit -32 ) * 5/9 );
+}
+
+const temp = 140;
+console.log(TemperatureFahrenheit(temp))

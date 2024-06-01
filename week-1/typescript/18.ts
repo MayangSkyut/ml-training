6aadba7437d8aa593f9a10a94589b42dcd053c5b
Marian Adonay <marianadonay40@gmail.com>
2024-06-01 15:46:20 +0800

    [TS-18]Create a shopping cart that allows the user to add items to the cart and remove items from the cart. An item is a javascript object with the following properties:  * - name: the name of the item  * - price: the price of the item

--- week-1/typescript/18.ts
+++ week-1/typescript/18.ts
@@ -16,3 +16,76 @@
  * @param {string[]} items
  * @returns {string}
  */
+
+/**
+ * Create a shopping cart that allows the user to add items to the cart and remove items from the cart. An item is a javascript object with the following properties:
+ * - name: the name of the item
+ * - price: the price of the item
+ *
+ * The shopping cart should be able to store up to 10 items.
+ * The shopping cart should be able to display the total price of the items in the cart.
+ * The shopping cart should be able to display the total number of items in the cart.
+ *
+ * Example:
+ * - add item "apple" to cart
+ * - remove item "apple" from cart
+ * - display total price of items in cart
+ * - display total number of items in cart
+ *
+ * @param {string[]} items
+ * @returns {string}
+ */
+
+
+interface ShoppingCartItem {
+    name: string;
+    price: number;
+  }
+  
+interface ShoppingCart {
+    addItem: (item: ShoppingCartItem) => string;
+    removeItem: (itemName: string) => string;
+    getTotalPrice: () => number;
+    getTotalItems: () => number;
+  }
+  
+  export function createShoppingCart(): ShoppingCart {
+    const items: ShoppingCartItem[] = [];
+  
+    return {
+      addItem: (item) => {
+        if (items.length === 10) {
+          return "Cart is full, cannot add more items.";
+        }
+  
+        items.push(item);
+        return `Added ${item.name} to the cart.`;
+      },
+      removeItem: (itemName) => {
+        const itemIndex = items.findIndex((item) => item.name === itemName);
+        if (itemIndex === -1) {
+          return `${itemName} is not in the cart.`;
+        }
+  
+        const removedItem = items.splice(itemIndex, 1)[0];
+        return `Removed ${removedItem.name} from the cart.`;
+      },
+      getTotalPrice: () => {
+        return items.reduce((total, item) => total + item.price, 0);
+      },
+      getTotalItems: () => {
+        return items.length;
+      },
+    };
+  }
+  
+
+  const cart = createShoppingCart();
+
+console.log(cart.addItem({ name: 'Item 1', price: 10 }));
+console.log(cart.addItem({ name: 'Item 2', price: 20 }));
+console.log(cart.getTotalPrice());
+console.log(cart.getTotalItems());
+console.log(cart.removeItem('Item 1'));
+console.log(cart.getTotalPrice());
+console.log(cart.getTotalItems());

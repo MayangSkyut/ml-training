d3ed2a3d79a489be9a5afedc15369d9064cac331
Marian Adonay <marianadonay40@gmail.com>
2024-06-01 09:40:13 +0800

    [TS-2]  Write a function to calculate the sum of all numbers in an array.

--- week-1/typescript/2.ts
+++ week-1/typescript/2.ts
@@ -3,3 +3,16 @@
  * @param {number[]} arr The array of numbers.
  * @return {number} The sum of all numbers in the array.
  */
+
+
+function Calculate(arr: number[]): number {
+    let total : number = 0;
+    for (let i = 0; i < arr.length; i++) {
+      total += arr[i];
+    }
+    return total;
+  }
+  
+  const numberni = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const sum1 = Calculate(numberni);
+  console.log(sum1); 

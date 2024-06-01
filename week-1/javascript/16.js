60af0e02e36f3e78912a8714169b885569d3b140
Marian Adonay <marianadonay40@gmail.com>
2024-06-01 08:06:47 +0800

    [JS-16]Computes the average marks of the  students

--- week-1/javascript/16.js
+++ week-1/javascript/16.js
@@ -8,17 +8,48 @@
  *
  * Grade computed as follows:
  * < 60 : F
- * 60-70 : D
- * 70-80 : C
- * 80-90 : B
- * > 90 : A
+ * 60-69 : D
+ * 70-79 : C
+ * 80-89 : B
+ * >= 90 : A
  * @return {number} The average marks of the students.
  */
 
-const students = [
+// const students = [
+//   { name: "John", mark: 90 },
+//   { name: "Jane", mark: 85 },
+//   { name: "Joe", mark: 95 },
+//   { name: "Tom", mark: 65 },
+//   { name: "Nancy", mark: 75 },
+// ];
+
+
+function Average(){
+const studentMarks = [
   { name: "John", mark: 90 },
   { name: "Jane", mark: 85 },
   { name: "Joe", mark: 95 },
   { name: "Tom", mark: 65 },
   { name: "Nancy", mark: 75 },
 ];
+
+
+for (const index of studentMarks) {
+  let grade;
+  if(index.mark < 60){
+    grade = 'F'
+  } else if (index.mark >= 60 && index.mark <= 69){
+    grade = 'D'
+  }else if (index.mark >= 70 && index.mark <= 79){
+    grade = 'C'
+  }else if (index.mark >= 80 && index.mark <= 89){
+    grade = 'B'
+  }else {
+    grade ='A'
+  }
+console.log(`Name of student: ${index.name}, Grade: ${grade}`);
+  
+}
+
+}
+Average();

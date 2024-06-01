2b4a2bf3a9165067f1d1f17c8f6837770455b04e
Marian Adonay <marianadonay40@gmail.com>
2024-06-01 10:27:46 +0800

    [TS-4] Write a function to display the current date and time in the format  * 'MM-DD-YYYY HH:MM:SS'.

--- week-1/typescript/4.ts
+++ week-1/typescript/4.ts
@@ -3,3 +3,21 @@
  * 'MM-DD-YYYY HH:MM:SS'.
  * @return {string} The current date and time in the format 'MM-DD-YYYY HH:MM:SS'.
  */
+
+
+function CurrentDate(){
+    const date : Date = new Date();
+    const month = date.getMonth();
+    const day = date.getDay();
+    const year= date.getFullYear();
+
+    const hours=date.getHours();
+    const minutes=date.getMinutes();
+    const seconds=date.getSeconds();
+
+    const CurrentDateFormat: string = `${month} - ${day} - ${year}  ${hours}: ${minutes}: ${seconds}` 
+
+    console.log(CurrentDateFormat)
+}
+
+CurrentDate();

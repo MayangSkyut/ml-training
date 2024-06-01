e20e3e087edbd82b737d23bb1fc34504f721bdd5
Marian Adonay <marianadonay40@gmail.com>
2024-06-01 15:39:21 +0800

    [TS-16] computes the average marks of the following students

--- week-1/typescript/16.ts
+++ week-1/typescript/16.ts
@@ -15,10 +15,56 @@
  * @return {number} The average marks of the students.
  */
 
-const students = [
-  { name: "John", mark: 90 },
-  { name: "Jane", mark: 85 },
-  { name: "Joe", mark: 95 },
-  { name: "Tom", mark: 65 },
-  { name: "Nancy", mark: 75 },
-];
+// const students = [
+//   { name: "John", mark: 90 },
+//   { name: "Jane", mark: 85 },
+//   { name: "Joe", mark: 95 },
+//   { name: "Tom", mark: 65 },
+//   { name: "Nancy", mark: 75 },
+// ];
+
+
+
+function getStudentMarks(): { name: string; mark: number }[] {
+  return [
+    { name: "John", mark: 90 },
+    { name: "Jane", mark: 85 },
+    { name: "Joe", mark: 95 },
+    { name: "Tom", mark: 65 },
+    { name: "Nancy", mark: 75 },
+  ];
+}
+
+function getGrade(mark: number): string {
+  if (mark < 60) {
+    return "F";
+  } else if (mark >= 60 && mark <= 69) {
+    return "D";
+  } else if (mark >= 70 && mark <= 79) {
+    return "C";
+  } else if (mark >= 80 && mark <= 89) {
+    return "B";
+  } else {
+    return "A";
+  }
+}
+
+function calculateAverage(): number {
+  let totalMarks = 0;
+  const studentMarks = getStudentMarks();
+  for (const student of studentMarks) {
+    totalMarks += student.mark;
+  }
+  return totalMarks / studentMarks.length;
+}
+
+function printGrades(): void {
+  getStudentMarks().forEach(({ name, mark }) => {
+    console.log(`Name of student: ${name}, Grade: ${getGrade(mark)}`);
+  });
+}
+
+
+console.log(`The average marks of the students is: ${calculateAverage()}`);
+printGrades();
+  

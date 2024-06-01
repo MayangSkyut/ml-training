4b86d6f803848d9bdba472ba0f9414f66a38a4c1
Marian Adonay <marianadonay40@gmail.com>
2024-06-01 11:32:13 +0800

    [TS-8] Write a function that returns all vowels in a string.

--- week-1/typescript/8.ts
+++ week-1/typescript/8.ts
@@ -3,3 +3,20 @@
  * @param {string} str The string to check.
  * @return {string} The string with all vowels removed.
  */
+
+
+
+function vowels (str: string): string{
+    const vowels : string [] = ["a", "e","i","o", "u" ];
+    let value : string  = '';
+
+    for (let i of str.toLowerCase()){
+        if(vowels.includes(i)){
+            value += i
+        }
+    }
+    return value;
+
+}
+
+console.log(vowels("Hi!this is Marian Adonay"));

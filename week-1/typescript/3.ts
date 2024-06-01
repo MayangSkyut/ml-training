9d2bfaed94e61851535f5654547c57d0b4abaf33
Marian Adonay <marianadonay40@gmail.com>
2024-06-01 10:16:27 +0800

    [TS-3] Write a function that prints the numbers from 1 to a given number.

--- week-1/typescript/3.ts
+++ week-1/typescript/3.ts
@@ -5,3 +5,23 @@
  * For numbers which are multiples of both three and five print 'FizzBuzz'.
  * @param {number} n The number to print.
  */
+
+
+function numbers(n: number){
+    for (let i = 0; i <= n; i++){
+        if (i %3 === 0 && i %5 === 0){
+            console.log("FizzBuzz")
+        }
+        else if(i %5 === 0){
+            console.log("Buzz")
+        }
+        else if  (i %3 === 0){
+            console.log("Fizz")
+        }
+        else {
+            console.log(i)
+        }
+    }
+}
+
+(numbers(14));
